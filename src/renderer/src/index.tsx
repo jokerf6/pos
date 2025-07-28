67540b1e83d3d40352b31e96cc7642da0306dbe2
mahmoudbahaa755 <mahmoudbahaa755@gmail.com>
2025-07-28 22:59:16 +0300

    .

--- src/renderer/src/index.tsx
+++ src/renderer/src/index.tsx
@@ -6,7 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import store from "./store";
 import { HashRouter } from "react-router-dom";
-
+import "./i18n";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
